abb7f5ff2a6b0113f23d979b5272906fdbeb3281
Jeff Lamb <jrlambs@gmail.com>
2017-03-04 23:33:39 -0500

    Switch to lists and divs for schedule, not as pretty yet and not done.. baby steps

--- src/main/webapp/js/rcrdit.js
+++ src/main/webapp/js/rcrdit.js
@@ -5,7 +5,7 @@
  */
 
 $( document ).ready(function() {
-    getSchedule(null);
+    getSchedule2(null);
 });
 
 function padToTwoDigits(ss){
@@ -14,6 +14,84 @@ function padToTwoDigits(ss){
         ss= "0"+ss;
     }
     return ss;
+}
+
+
+function getSchedule2(requestObject){
+    if(isNull(requestObject)){
+        requestObject = {"channelsPerPage" : "4000", "pageNum" : "1"};
+    }
+    $.ajax({
+        url: 'rest/getSchedule',
+        type: 'post',
+        dataType: 'json',
+        contentType: "application/json",
+        data: JSON.stringify(requestObject),
+        success: function (data) {
+            var channelList = data.schedule;
+            var requestObject = data.requestObject;
+            var requestStartTimeEpochSeconds = requestObject.startTime.epochSecond;
+            var requestEndTimeEpochSeconds = requestObject.endTime.epochSecond;
+            
+            
+            $("#guideGoesHere").html("");
+            var channelUl = $("<ul></ul>");
+            var channelGroupsUl = $("<ul></ul>").attr("id","channelGroups").append($("<li></li>").append(channelUl));
+            for(var idx in channelList){
+                var individualChannelLi = $("<li></li>");
+                var channel = channelList[idx];
+                var currentPercentOver = 0;
+                var channelProgramUl = $("<ul></ul>").attr("channelNum",channel.displayName);
+                individualChannelLi.append(channelProgramUl);
+                var channelNameLi = $("<li class='channel'></li>");
+                channelNameLi.append("<span></span>").addClass("channelNumLabel").append(channel.displayName);
+                channelProgramUl.append(channelNameLi);
+                
+                var programsLi = $("<li></li>").addClass("programs");
+                for(var idx2 in channel.programs){
+                    var program = channel.programs[idx2];
+                    programsLi.append(getProgramDiv(program,requestObject,currentPercentOver));
+                    currentPercentOver += program.widthPercent;
+                }
+                channelProgramUl.append(programsLi);
+                channelUl.append(individualChannelLi);
+            }
+            $("#guideGoesHere").append(channelGroupsUl);
+        },
+        error:  function ( jqXHR, textStatus, errorThrown ){
+            alert(errorThrown);
+        }
+    });
+}
+
+function getProgramDiv(program, requestObject, startAtPercent){
+    var programDiv = $("<div></div>").addClass("channelProgram");
+    var titleInfoDiv = $("<div></div>").addClass("titleInfo");
+    var otherStuffDiv = $("<div></div>").addClass("otherProgramStuff");
+    titleInfoDiv.append(program.title);
+    if(!isNull(program.subTitle)){
+        titleInfoDiv.append($("<p>"+program.subTitle+"</p>").addClass("programSubtitle"));
+    }
+    programDiv.append(titleInfoDiv);
+    programDiv.append(otherStuffDiv);
+    
+    var requestStartTimeEpochSeconds = requestObject.startTime.epochSecond;
+    var requestEndTimeEpochSeconds = requestObject.endTime.epochSecond;
+    var showingMinutes = (requestEndTimeEpochSeconds-requestStartTimeEpochSeconds)/60;
+    var durationSeconds = program.stop.epochSecond-program.start.epochSecond;
+    var displayDurationSeconds = durationSeconds;
+    if(program.start.epochSecond < requestStartTimeEpochSeconds){
+        displayDurationSeconds = displayDurationSeconds-(requestStartTimeEpochSeconds-program.start.epochSecond);
+    }
+    if(program.stop.epochSecond > requestEndTimeEpochSeconds){
+        displayDurationSeconds = displayDurationSeconds-(program.stop.epochSecond-requestEndTimeEpochSeconds);
+    }
+    var displayDurationMinutes = displayDurationSeconds/60;
+    
+    var widthPercent = (displayDurationMinutes/showingMinutes)*100;
+    programDiv.attr("style","left: "+startAtPercent+"%; width: "+widthPercent+"%");
+    program.widthPercent = widthPercent;
+    return programDiv;
 }
 
 
